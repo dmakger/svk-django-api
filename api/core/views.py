59a3b65a2171cf2925c245f28563f6b850b88734
dmakger <dmakger@yandex.ru>
2023-08-24 17:45:59 +0300

    Create a view for Brand Partner

--- api/core/views.py
+++ api/core/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import BrandPartner
+
+
+# ===========
+# Партнеры бренда
+# ===========
+class BrandPartnerView(viewsets.ModelViewSet):
+    # serializer_class = MovieSerializer
+    queryset = BrandPartner.objects.all()
+    permission_classes = [permissions.AllowAny]
+
+    # error_helper = BrandPartnerError()
+
+    @action(methods=['get'], detail=False)
+    def get_brand_partners(self, request):
+        return Response(
+            self.serializer_class(self.queryset).data,
+            status=status.HTTP_200_OK,
+        )
